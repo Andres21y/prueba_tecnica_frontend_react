b97aa72ccd693e7e5a29cb8692726e92afc78c2e
Andres21y <andres17vh@gmail.com>
2025-12-29 16:33:41 -0500

    feat: adding AuthContext for global session state and inrteface file

---
+++ src/context/authContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from "react";
+import type { AuthContextType } from "../utils/interface";
+
+/**creacion del contexto */
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
+
+    /**funcion para guardar el token en local storage */
+    const login = (newToken: string) => {
+        localStorage.setItem('token', newToken);
+        setToken(newToken)
+    }
+
+    /**funcion que elimina el token y limpia el estado */
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken(null)
+    }
+
+    /**indicamos si el user está autenticado */
+    const isAuthenticated = !!token;
+
+    return (
+        <AuthContext.Provider
+            value={{
+                token,
+                login,
+                logout,
+                isAuthenticated
+            }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error('useAuth must be used within AuthProvider')
+}
